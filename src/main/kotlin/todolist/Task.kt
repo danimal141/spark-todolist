dbdc34ba3ce890bffc361136c4aaa7e4a4d2ef04
danimal141 <hideaki.ishii1204@gmail.com>
2019-09-10 22:37:39 +0900

    Add Task

---
+++ src/main/kotlin/todolist/Task.kt
@@ -0,0 +1,9 @@
+package todolist
+
+/**
+ * Created by hideaki on 2019/09/10.
+ */
+data class Task(
+    val id: Long,
+    val content: String,
+    val done: Boolean)
--- src/main/kotlin/todolist/main.kt
+++ src/main/kotlin/todolist/main.kt
@@ -4,10 +4,18 @@ package todolist
  * Created by hideaki on 2019/09/08.
  */
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import spark.Spark.get
 
 fun main(args: Array<String>) {
-    get("/hello") { _, _ ->
-        "Hello, world"
+    val objectMapper = ObjectMapper().registerKotlinModule()
+    get("/tasks") { _, _ ->
+        // TODO: replace
+        val tasks = listOf(
+            Task(1, "Test1", false),
+            Task(2, "Test2", true)
+        )
+        objectMapper.writeValueAsString(tasks)
     }
 }
