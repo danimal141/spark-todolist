5e6afa8e8333d97d0dbaffc0250a67565133af25
danimal141 <hideaki.ishii1204@gmail.com>
2019-09-11 22:57:46 +0900

    Improve format

--- src/main/kotlin/todolist/functions.kt
+++ src/main/kotlin/todolist/functions.kt
@@ -5,9 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper
 /**
  * Created by hideaki on 2019/09/11.
  */
-inline fun <reified T: Any> ObjectMapper.readValue(src: ByteArray): T? =
+inline fun <reified T : Any> ObjectMapper.readValue(src: ByteArray): T? =
     try {
         readValue(src, T::class.java)
-    } catch(e: Exception) {
+    } catch (e: Exception) {
         null
     }
